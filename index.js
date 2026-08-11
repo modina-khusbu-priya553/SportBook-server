f25e6122e6ecabaf67c3a63828a49c5658108c6f
Modina Khusbu Priya <modinakhusbupriya@gmail.com>
2026-08-11 03:57:37 +0300

    implement middlewate

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express()
 const cors = require("cors");
 const dotenv = require('dotenv')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet } = require("jose-cjs");
 dotenv.config()
+
+
 
 // const uri = process.env.MONGODB_URI;
 const port = process.env.PORT;
@@ -21,6 +24,34 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+const JWKS = createRemoteJWKSet(
+   new URL('https://sport-book-ygve.vercel.app//api/auth/jwks')
+)
+
+// middleware
+const verifyToken = async(req, res, next) => {
+  const authHeader = req?.headers.authorization
+
+  if(!authHeader) {
+    return res.status(401).json({ message:
+      "Unauthorized"
+    });
+  }
+  const token = authHeader.split(" ")[1];
+
+  if(!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS)
+    next()
+  } catch (error) {
+   return res.status(401).json({ message: "Forbidden" }); 
+  }
+  
+};
 
 const run = async () => {
   try {
@@ -41,7 +72,7 @@ const run = async () => {
     })
 
     // 2: get data from database by id
-    app.get('/facilities/:id', async (req, res) => {
+    app.get('/facilities/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await facilitiesCollection.findOne(query);
